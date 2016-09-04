224d45f58476b0d864753d7e8ac73af861b95cc0
Clément Nerma <clement.nerma@gmail.com>
2016-09-04 16:46:52 +0200

    Added .getExtension .open in dolphin shared library

--- apps/dolphin/shared-lib.js
+++ apps/dolphin/shared-lib.js
@@ -8,6 +8,15 @@
 
 'use strict';
 
+/** The filesystem module
+  * @type {object} */
+const fs = require('fs');
+
+// If the filesystem module failed to load...
+if(e(fs))
+  // Error
+  return new NightError('Failed to load the "fs" module');
+
 /** The registry module
   * @type {object} */
 const reg = require('registry');
@@ -20,6 +29,37 @@ if(e(reg))
 /** A <NightDocument> instance for work
   * @type {NightDocument} */
 const doc = new NightDocument();
+
+/**
+  * Get the registry extension of an item
+  * @param {string} path
+  * @returns {string}
+  */
+$export.getExtension = (path) => {
+  // If the item doesn't exist...
+  if(fs.exists(path) === false)
+    return 'unknown';
+
+  // If that's a folder...
+  if(fs.dirExists(path) === true)
+    return 'directory';
+
+  // If that's a file...
+  if(fs.fileExists(path) === true) {
+    /** The file's extension
+      * @type {string|void} */
+    let ext = Night.getExtension(path);
+
+    // If an extension was found...
+    if(ext)
+      return '.' + ext;
+    else // Else...
+      return 'unknown';
+  }
+
+  // Unknown file type (maybe a bug)
+  return 'unknown';
+};
 
 /**
   * Get informations about a file type
@@ -41,3 +81,10 @@ $export.getIcon = filetype => ($export.getFileType(filetype) || {}).icon;
   * @returns {string|void}
   */
 $export.getDescription = filetype => ($export.getFileType(filetype) || {}).descriptor;
+
+/**
+  * Open a file
+  * @param {string} path
+  * @returns {boolean|NightError} True if succeed
+  */
+$export.open = path => Night.openFile(path);
