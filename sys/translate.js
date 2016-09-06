fce52c479f00e5dcd57bd1888f52eb952b50b7e6
Clément Nerma <clement.nerma@gmail.com>
2016-09-06 21:24:21 +0200

    Fixed a bug with translation package loader
    Which was using UI.showError to show a modal when an error happens ; but
    applications must be able to control the errors themselves

--- sys/translate.js
+++ sys/translate.js
@@ -56,17 +56,13 @@ const load_translation = (lang, setAsCurrent = false) => {
   // Try to load the package file
   try { pkg = fs.readFileSync(n('/sys/data/translations/' + lang + '.ntp', true), SYSTEM_ENCODING); }
   catch(e) {
-    let err = new NightError('Failed to load the translation package file for "${lang}"', {lang, jsError: e});
-    UI.showError(err);
-    return err;
+    return new NightError('Failed to load the translation package file for "${lang}"', {lang, jsError: e});
   }
 
   // Parse it
   try { pkg = JSON.parse(pkg); }
   catch(e) {
-    let err = new NightError('Failed to parse the translation package, not a valid JSON file for "${lang}"', {lang, jsError: e});
-    UI.showError(err);
-    return err;
+    return new NightError('Failed to parse the translation package, not a valid JSON file for "${lang}"', {lang, jsError: e});
   }
 
   // If asked, set this language as the current one
